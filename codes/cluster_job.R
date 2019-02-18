b4bd8de9382b72a6f450aeaf01b6c442224e85e4
fedfer <ff31@duke.edu>
2019-02-18 09:50:59 -0500

    S = 2

--- codes/cluster_job.R
+++ codes/cluster_job.R
@@ -62,7 +62,7 @@ if(type_model == 0){
 }
 
 
-S = 1
+S = 2
 err = err_pred = FR = err_beta = matrix(0,ncol = 8, nrow = S)
 alpha = 0.05
 TP_main = TN_main = TP_int = TN_int = matrix(0,ncol = 8, nrow = S)
