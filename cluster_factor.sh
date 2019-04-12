51ced8426e40f50d54c706812ee1289402d2cca2
Federico Ferrari <ff31@dcc-slogin-01.oit.duke.edu>
2019-04-12 09:07:17 -0400

    add batch scripts

---
+++ cluster_factor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --partition=herringlab-low,statdept-low
+#SBATCH --account=statdept
+#SBATCH -c1
+
+singularity run -B /work/sta790/ff31:/work/sta790/ff31 felpo_ex.simg factor_interactions/codes/cluster_longeneker.R 4000 3000 1
+
